22c7db714be61c6261a2da8bf26b4b71669e192f
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-04-16 09:35:58 +0000

    Added User schema to models.py

--- trailfinders/models.py
+++ trailfinders/models.py
@@ -1,1 +1,12 @@
 from trailfinders import db
+
+class User(db.Model):
+    """ 
+    schema for User model
+    """
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, unique=True, nullable=False)                                                                                    
