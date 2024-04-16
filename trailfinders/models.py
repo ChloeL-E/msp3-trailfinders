f20c4603e4ccc41a810c25c4d25da40663148121
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-04-16 09:49:20 +0000

    Added Hike and Category schema to models.py

--- trailfinders/models.py
+++ trailfinders/models.py
@@ -1,7 +1,8 @@
 from trailfinders import db
 
+
 class User(db.Model):
-    """ 
+    """
     schema for User model
     """
     user_id = db.Column(db.Integer, primary_key=True)
@@ -9,4 +10,25 @@ class User(db.Model):
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
-    password = db.Column(db.String, unique=True, nullable=False)                                                                                    
+    password = db.Column(db.String, unique=True, nullable=False)
+
+
+class Hike(db.Model):
+    """
+    schema for Category model
+    """ 
+    hike_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String)
+    distance = db.Column(db.String, nullable=False)
+    elevation = db.Column(db.String, nullable=False)
+    difficulty = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    category_id = db.Column(db.String, nullable=False, foreign_key=True)
+
+    class Category(db.Model):
+        """
+        schema for Category model
+        """
+        category_id = db.Column(db.Integer, primary_key=True)
+        type = db.Column(db.String, nullable=False)
