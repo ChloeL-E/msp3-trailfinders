9a1c467f06d78bdb09ee99664ee3449efb08f56b
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-04-23 13:03:03 +0000

    Fixed issue with hero-image filepath

--- trailfinders/routes.py
+++ trailfinders/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from trailfinders import app, db
+from trailfinders.models import User, Hike, Category
 
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("index.html")
