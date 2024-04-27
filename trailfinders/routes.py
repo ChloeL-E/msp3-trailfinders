9622b1b469aad4112d96f6068ec6b5e9867fff76
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-04-27 19:05:57 +0000

    Imported flask_login, created get_user function to load current user

--- trailfinders/routes.py
+++ trailfinders/routes.py
@@ -1,6 +1,23 @@
 from flask import render_template, request, redirect, url_for
 from trailfinders import app, db
 from trailfinders.models import User, Hike, Category
+import flask_login as fl
+
+login_manager = fl.LoginManager(app)
+
+@login_manager.user_loader
+def get_user(id):
+    """
+    Loads the current user
+
+    Arguments: 
+        id- the primary key on the User table
+
+    Returns:
+        A user object from a query on user table
+    """
+    return User.query.get(int(id))
+
 
 
 @app.route("/")
