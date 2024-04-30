ba19123f956e92edfc96cb98a705cbc106f083ae
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-04-30 19:20:50 +0000

    Added 404, added JS function to navbar scroll

--- trailfinders/static/js/script.js
+++ trailfinders/static/js/script.js
@@ -1,4 +1,30 @@
-// Initialization for ES Users - Bootrap search bar
-import { Input, Ripple, initMDB } from "mdb-ui-kit";
+// keep track of previous scroll position
+let prevScrollPos = window.pageYOffset;
 
-initMDB({ Input, Ripple });
+window.addEventListener('scroll', function () {
+    // current scroll position
+    const currentScrollPos = window.pageYOffset;
+
+    if (prevScrollPos > currentScrollPos) {
+        // user has scrolled up
+        document.querySelector('.navbar').classList.add('show');
+    } else {
+        // user has scrolled down
+        document.querySelector('.navbar').classList.remove('show');
+    }
+
+    // update previous scroll position
+    prevScrollPos = currentScrollPos;
+});
+
+// Initialization for ES Users - Bootrap search bar
+import {
+    Input,
+    Ripple,
+    initMDB
+} from "mdb-ui-kit";
+
+initMDB({
+    Input,
+    Ripple
+});
