84285f8f2b4326f00baafcbf789774575aadfa94
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-06-05 12:09:26 +0000

    Checked and added to routes comments

--- trailfinders/routes.py
+++ trailfinders/routes.py
@@ -4,7 +4,7 @@ from trailfinders import app, db
 from trailfinders.models import User, Hike, Category
 
 
-# index.html
+# Home
 @app.route("/")
 def home():
     """
@@ -17,7 +17,7 @@ def home():
     return render_template("index.html", hikes=hikes)
 
 
-# register.html
+# Register
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """
@@ -58,6 +58,7 @@ def register():
     return render_template("register.html")
 
 
+# Login
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """
@@ -72,6 +73,7 @@ def login():
        message
     """
     if request.method == "POST":
+        # Get username and password inputs from form
         username = request.form.get("username")
         password = request.form.get("password")
 
@@ -79,7 +81,7 @@ def login():
             # Username &/or password is missing
             flash("Please enter a username and password")
             return redirect(url_for("login"))  # Redirect to login page
-
+        # Checks for existing user with username
         existing_user = User.query.filter_by(username=username.lower()).first()
         if existing_user and check_password_hash(existing_user.password,
                                                  password):
@@ -91,12 +93,12 @@ def login():
         else:
             # Incorrect password, redirect to login
             flash("Invalid username and/or password, please try again")
-            return redirect(url_for("login"))  # redirect to login
+            return redirect(url_for("login")) 
 
     return render_template("login.html")
 
 
-# logout.html
+# Logout
 @app.route("/logout")
 def logout():
     """
@@ -107,7 +109,7 @@ def logout():
     return redirect(url_for("home"))  # Redirect to home
 
 
-#  category.html
+#  Category
 @app.route("/categories")
 def categories():
     """
@@ -118,7 +120,7 @@ def categories():
     return render_template("categories.html", categories=categories)
 
 
-# add a new category
+# Add a new category
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     """
@@ -129,9 +131,8 @@ def add_category():
     Redirects user to categories page
     """
     if request.method == "POST":
-        # Get the current user
+        # Get the current user and the catgeory name from the form
         current_user = User.query.filter_by(username=session["user"]).first()
-        # Get the category name from the form
         category_name = request.form.get("category_name")
         # Create a new category with the current user's ID
         category = Category(category_name=category_name,
@@ -143,7 +144,7 @@ def add_category():
     return render_template("add_category.html")
 
 
-#  edit a category
+#  Edit a category
 @app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
     """
@@ -160,13 +161,14 @@ def edit_category(category_id):
         flash("You do not have permission to edit this category")
         return redirect(url_for("categories"))
     if request.method == "POST":
+        # get the catgeory name from the form, update category name and commit to DB
         category.category_name = request.form.get("category_name")
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("edit_category.html", category=category)
 
 
-#  delete a category
+#  Delete a category
 @app.route("/delete_category/<int:category_id>", methods=["GET", "POST"])
 def delete_category(category_id):
     """
@@ -180,10 +182,12 @@ def delete_category(category_id):
     current_user = User.query.filter_by(username=session["user"]).first()
     category = Category.query.get_or_404(category_id)
     #  defensive programming to prevent other users deleting category
+    # if current user did not create post, unable to delete, flash message
     if current_user.id != category.created_by:
         flash("You do not have permission to delete this category")
         return redirect(url_for("categories"))
     else:
+        # get the category, delete it and commit changes to db
         category = Category.query.get_or_404(category_id)
         db.session.delete(category)
         db.session.commit()
@@ -191,7 +195,7 @@ def delete_category(category_id):
         return redirect(url_for("categories"))
 
 
-#  my_hikes
+#  Hikes
 @app.route("/my_hikes")
 def my_hikes():
     """
@@ -202,7 +206,7 @@ def my_hikes():
     return render_template("my_hikes.html", hikes=hikes)
 
 
-#  add a new hike
+#  Add a new hike
 @app.route("/add_hike", methods=["GET", "POST"])
 def add_hike():
     """
@@ -240,7 +244,7 @@ def add_hike():
     return render_template("add_hike.html", categories=categories)
 
 
-#  edit a hike
+#  Edit a hike
 @app.route("/edit_hike/<int:hike_id>", methods=["GET", "POST"])
 def edit_hike(hike_id):
     """
@@ -256,6 +260,7 @@ def edit_hike(hike_id):
     current_user = User.query.filter_by(username=session["user"]).first()
     hike = Hike.query.get_or_404(hike_id)
     if request.method == "POST":
+        # get the data from the form inputs
         hike.hike_title = request.form.get("hike_title")
         hike.image_url = request.form.get("image_url")
         hike.distance = request.form.get("distance")
@@ -264,13 +269,13 @@ def edit_hike(hike_id):
         hike.description = request.form.get("description")
         hike.category_id = request.form.get("category_id")
         hike.user_id = current_user.id  # Assign the current user's ID
-        db.session.commit()
+        db.session.commit() # commit changes to DB
         return redirect(url_for("my_hikes"))
     return render_template("edit_hike.html",
                            hike=hike, categories=categories)
 
 
-#  delete a hike
+#  Delete a hike
 @app.route("/delete_hike/<int:hike_id>", methods=["GET", "POST"])
 def delete_hike(hike_id):
     """
@@ -288,6 +293,7 @@ def delete_hike(hike_id):
         flash("You do not have permission to delete this Hike")
         return redirect(url_for("my_hikes"))
     else:
+        # get the hike by ID if available, delete and commit to DB
         hike = Hike.query.get_or_404(hike_id)
         db.session.delete(hike)
         db.session.commit()
@@ -295,7 +301,8 @@ def delete_hike(hike_id):
         return redirect(url_for("my_hikes"))
 
 
-# Guidance on Error handler routes taken from Geeks for Geeks 
+# Guidance on Error handlers taken from Geeks for Geeks
+# 404
 @app.errorhandler(404)
 def handle_404(e):
     """
@@ -303,10 +310,10 @@ def handle_404(e):
     """
     return render_template("404.html"), 404
 
-
+# 500
 @app.errorhandler(500)
 def handle_500(e):
     """
-    Handles 500 error, Internal server error
+    Handles 500 error, Internal server error 
     """
     return render_template("500.html"), 500
